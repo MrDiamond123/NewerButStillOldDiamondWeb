c001674599e0f28b74832c0e8ec460f68b794038
Mr_Diamond123 <kurtisiscool123@gmail.com>
2017-03-01 10:22:13 -0500

    Update dice.js

--- scripts/dice.js
+++ scripts/dice.js
@@ -1,1 +1,18 @@
-
+ var dice_value;
+  function roll_dice() {
+    dice_value = Math.floor(Math.random() * 6) + 1;     // returns a number between 1 and 6
+            if (dice_value == 1) {
+        document.getElementById('dice').src='assets/Dice1.png';
+     } else if (dice_value == 2) {
+        document.getElementById('dice').src='assets/Dice2.png';
+     } else if (dice_value == 3) {
+        document.getElementById('dice').src='assets/Dice3.png';
+     } else if (dice_value == 4) {
+        document.getElementById('dice').src='assets/Dice4.png';
+     } else if (dice_value == 5) {
+        document.getElementById('dice').src='assets/Dice5.png';
+     } else if (dice_value == 6) {
+        document.getElementById('dice').src='assets/Dice6.png';
+     }
+    
+  }
