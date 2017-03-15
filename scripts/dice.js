f4140f47b87e02bd4facaeb9b9903c0c8d6dc417
Mr_Diamond123 <kurtisiscool123@gmail.com>
2017-03-15 10:17:38 -0400

    Update dice.js

--- scripts/dice.js
+++ scripts/dice.js
@@ -23,15 +23,15 @@ function set_Dice(diceValue) {
  }
 
  function cycle_Dice() {
-  setTimeout (set_Dice(6), 60);
+  setTimeout (set_Dice(6), 600);
     
-  setTimeout (set_Dice(5), 50);
+  setTimeout (set_Dice(5), 500);
     
-  setTimeout (set_Dice(4), 40);
+  setTimeout (set_Dice(4), 400);
     
-  setTimeout (set_Dice(3), 30);
+  setTimeout (set_Dice(3), 300);
     
-  setTimeout (set_Dice(2), 20);
+  setTimeout (set_Dice(2), 200);
     
   set_Dice(1);
   
