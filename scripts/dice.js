e6f6292dbe0b0848b2ab519a8d35375a7c0e3187
Mr_Diamond123 <kurtisiscool123@gmail.com>
2017-03-16 10:03:55 -0400

    Update dice.js

--- scripts/dice.js
+++ scripts/dice.js
@@ -68,7 +68,7 @@ function set_Dice(diceValue) {
 
   function roll_Dice() {
    dice_value = Math.floor(Math.random() * 6) + 1;     // returns a number between 1 and 6
-   setTimeout (check_Dice(), 1000);
+   setTimeout (check_Dice(), 3000);
    cycle_Dice();
      
    
