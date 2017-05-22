427600672194db30b503e8cfe3c71e2cc214795f
Mr_Diamond123 <kurtisiscool123@gmail.com>
2017-05-22 10:18:07 -0400

    Update mathDice.js

--- scripts/mathDice.js
+++ scripts/mathDice.js
@@ -1,1 +1,31 @@
+var dice1;
+var dice2;
+var problem;
+var answer;
+
+function giveProblem() {
+  
+  
+}
+
+function getAnswer(dice1, dice2, problem) {
+    if (problem = 1) {
+      answer = dice1 + dice2;
+      return answer;
+    }
+    if (problem = 2) {
+      answer = dice1 - dice2;
+      return answer;
+    }
+    if (problem = 3) {
+      answer = dice1 / dice2;
+      return answer;
+    }
+    if (problem = 4) {
+      answer = dice1 * dice2;
+      return answer;
+    }
+  
+}
+
 
