366b51d6d8dee5b04cffa7bd3697be54141e552f
Mr_Diamond123 <kurtisiscool123@gmail.com>
2017-05-22 10:23:54 -0400

    Update mathDice.js

--- scripts/mathDice.js
+++ scripts/mathDice.js
@@ -2,11 +2,10 @@ var dice1;
 var dice2;
 var problem;
 var answer;
+var display1 = document.getElementById('dice1');
+var display2 = document.getElementById('dice2');
+var display3 = document.getElementById('dice3');
 
-function giveProblem() {
-  
-  
-}
 
 function getAnswer(dice1, dice2, problem) {
     if (problem = 1) {
@@ -26,6 +25,21 @@ function getAnswer(dice1, dice2, problem) {
       return answer;
     }
   
+}
+
+function displayProblem() {
+}  
+
+
+
+
+function setProblem() {
+  dice1 = Math.floor(Math.random() * 7);
+  dice2 = Math.floor(Math.random() * 7);
+  problem = Math.floor(Math.random() * 4);
+  if (getAnswer(dice1, dice2, problem) < 1) {
+    setProblem(); 
+  } else displayProblem();
 }
 
 
