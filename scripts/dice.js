10b1a8877ccdd8dedac3fca7d801c393cba7bdde
Mr_Diamond123 <kurtisiscool123@gmail.com>
2017-05-25 10:17:18 -0400

    UPDATE DICE ASSET SRC

--- scripts/dice.js
+++ scripts/dice.js
@@ -1,22 +1,22 @@
 var dice_value;
 function set_Dice(diceValue) {
    if (diceValue == 1) {
-        document.getElementById('dice').src='assets/Dice1.png';
+        document.getElementById('dice').src='assets/Dice1.svg';
         console.log("1");
      } else if (diceValue == 2) {
-        document.getElementById('dice').src='assets/Dice2.png';
+        document.getElementById('dice').src='assets/Dice2.svg';
         console.log("2");
      } else if (diceValue == 3) {
-        document.getElementById('dice').src='assets/Dice3.png';
+        document.getElementById('dice').src='assets/Dice3.svg';
         console.log("3");
      } else if (diceValue == 4) {
-        document.getElementById('dice').src='assets/Dice4.png';
+        document.getElementById('dice').src='assets/Dice4.svg';
         console.log("4");
      } else if (diceValue == 5) {
-        document.getElementById('dice').src='assets/Dice5.png';
+        document.getElementById('dice').src='assets/Dice5.svg';
         console.log("5");
      } else if (diceValue == 6) {
-        document.getElementById('dice').src='assets/Dice6.png';
+        document.getElementById('dice').src='assets/Dice6.svg';
         console.log("6");
      }
    
@@ -24,22 +24,22 @@ function set_Dice(diceValue) {
  function check_Dice() {
      dice_value = Math.floor(Math.random() * 6) + 1;     // returns a number between 1 and 6
      if (dice_value == 1) {
-        document.getElementById('dice').src='assets/Dice1.png';
+        document.getElementById('dice').src='assets/Dice1.svg';
         console.log("1");
      } else if (dice_value == 2) {
-        document.getElementById('dice').src='assets/Dice2.png';
+        document.getElementById('dice').src='assets/Dice2.svg';
         console.log("2");
      } else if (dice_value == 3) {
-        document.getElementById('dice').src='assets/Dice3.png';
+        document.getElementById('dice').src='assets/Dice3.svg';
         console.log("3");
      } else if (dice_value == 4) {
-        document.getElementById('dice').src='assets/Dice4.png';
+        document.getElementById('dice').src='assets/Dice4.svg';
         console.log("4");
      } else if (dice_value == 5) {
-        document.getElementById('dice').src='assets/Dice5.png';
+        document.getElementById('dice').src='assets/Dice5.svg';
         console.log("5");
      } else if (dice_value == 6) {
-        document.getElementById('dice').src='assets/Dice6.png';
+        document.getElementById('dice').src='assets/Dice6.svg';
         console.log("6");
      }
    
