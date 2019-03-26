fe7a0d09a551548b5d620b9ff3623b86036e81d4
Mr_Diamond123 <kurtisiscool123@gmail.com>
2019-03-26 13:24:19 -0400

    Update testDice.js

--- scripts/testDice.js
+++ scripts/testDice.js
@@ -75,8 +75,10 @@ function confirmAnswer() {
 function displayAnswerThing(boolean) {
   if (boolean) {
    answerThing.innerHTML = rightAnswer; 
+   answerThing.style.color = green;
   } else {
    answerThing.innerHTML = wrongAnswer; 
+   answerThing.style.color = red;
   }
 }
 
